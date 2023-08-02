2ca977d3cd8c4f2f8d5db6214442d340b937bc94
Abdul Riyan <riyanop786@gmail.com>
2023-08-02 02:14:54 -0700

    Add files via upload

--- Pokémon-Multiplier.cpp
+++ Pokémon-Multiplier.cpp
@@ -14,19 +14,9 @@ using namespace std;
 
 
 bool isPowerOfTwo(int n) {
-    if (n <= 0) {
-        return false;
-    }
+    
+    return n>0 && (n & (n-1))==0;
 
-    while (n % 2 == 0) {
-        n /= 2;
-    }
-
-    if(n==1){
-        return true;
-    }else{
-        return false;
-    }
 }
 
 
